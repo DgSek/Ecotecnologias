d43baa91c44ab00429557249f522d337ac01bb5b
DgSek <dgsek13@gmail.com>
2025-03-13 13:08:52 -0700

    basefire

---
+++ DB/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyldi4fvwcQUwTTvRJ_OgnVEUV9cSuLcw",
+  authDomain: "ecotecnologias-b04a9.firebaseapp.com",
+  projectId: "ecotecnologias-b04a9",
+  storageBucket: "ecotecnologias-b04a9.firebasestorage.app",
+  messagingSenderId: "172438962595",
+  appId: "1:172438962595:web:da9e125d089216bdf01a16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'ecotecnologias',
+  location: 'us-central1'
+};
+
